517074fdc3aad0933f1b4e24515057047b226bc8
John Rueda <johnrueda7@hotmail.com>
2024-11-04 22:06:27 -0500

    Se añade vista del login y dial en la pagina se añade mesnaje principal

--- UniqueStyleFrontend/src/app/app.component.ts
+++ UniqueStyleFrontend/src/app/app.component.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { BadgeModule } from 'primeng/badge';
+import { FieldsetModule } from 'primeng/fieldset';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ButtonModule],
+  imports: [CommonModule, FormsModule,  PasswordModule, FloatLabelModule, InputTextModule, DialogModule, RouterOutlet, ButtonModule, SpeedDialModule, BadgeModule, FieldsetModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
+
   title = 'UniqueStyleFrontend';
+
+  items!: MenuItem[];
+
+  elementsCart: any[] = []
+
+  visible: boolean = false;
+
+  password: string = '';
+  email: string = '';
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: object) { }
+
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        icon: 'pi pi-pencil',
+        command: () => {
+          // this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+          console.log('Add');
+
+        }
+      },
+    ];
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.showDialog();
+    }
+
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+
 }
