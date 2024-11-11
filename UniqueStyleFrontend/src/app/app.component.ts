be8eeaa3ee1581bf6e9f0279990468c15bfd2cb1
John Rueda <johnrueda7@hotmail.com>
2024-11-10 23:43:28 -0500

    Ajsute de tioenda con auth y peticones

--- UniqueStyleFrontend/src/app/app.component.ts
+++ UniqueStyleFrontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { SpeedDialModule } from 'primeng/speeddial';
 import { BadgeModule } from 'primeng/badge';
@@ -11,13 +11,20 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
 import { FormsModule } from '@angular/forms';
+import { UniqueStyleService } from './services/unique-style.service';
+import { ToastModule } from 'primeng/toast';
+import { SidebarModule } from 'primeng/sidebar';
+import { CarouselModule } from 'primeng/carousel';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule,  PasswordModule, FloatLabelModule, InputTextModule, DialogModule, RouterOutlet, ButtonModule, SpeedDialModule, BadgeModule, FieldsetModule],
+  imports: [CommonModule,CarouselModule,SidebarModule,ToastModule, FormsModule,  PasswordModule, FloatLabelModule, InputTextModule, DialogModule, RouterOutlet, ButtonModule, SpeedDialModule, BadgeModule, FieldsetModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  providers: [MessageService]
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
@@ -26,17 +33,35 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   items!: MenuItem[];
 
-  elementsCart: any[] = []
+  elementsCart: Product[] = []
 
   visible: boolean = false;
 
   password: string = '';
   email: string = '';
+
+  listProducts: Product[] = [];
   
-  constructor(@Inject(PLATFORM_ID) private platformId: object) { }
+  sidebarVisible: boolean = false;
+
+  responsiveOptions = [
+    { breakpoint: '1199px', numVisible: 1, numScroll: 1 },
+    { breakpoint: '991px', numVisible: 1, numScroll: 1 },
+    { breakpoint: '767px', numVisible: 1, numScroll: 1 }
+  ];
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private readonly uniqueStyleService: UniqueStyleService,
+    private messageService: MessageService
+  ) { }
 
 
   ngOnInit(): void {
+
+
+    // this.loadData()
+
     this.items = [
       {
         icon: 'pi pi-pencil',
@@ -60,4 +85,58 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.visible = true;
   }
 
+  onLogin() {
+    this.uniqueStyleService.login(this.email, this.password).then((res) => {
+      if (res) {
+        this.visible = false;
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Inicio de sesión exitoso' });
+        this.loadData();
+      }
+      else {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Usuario o contraseña incorrectos' });
+      }
+    });
+  }
+
+  loadData(){
+    this.uniqueStyleService.getProducts().subscribe((res: Product[]) => {
+      console.log(res);
+      this.listProducts = res;
+    });
+  }
+
+  addCart(product: Product){
+    this.elementsCart.push(product);
+  }
+
+  showSide(){
+    this.sidebarVisible = true;
+  }
+
+  closeSide(){
+    this.sidebarVisible = false;
+  }
+
+  buyProduct(){
+
+    if(this.elementsCart.length === 0){
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No hay productos en el carrito' });
+      return;
+    }else{
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Compra realizada con éxito' });
+      this.elementsCart = [];
+      this.sidebarVisible = false;
+    }
+
+  }
+
+}
+
+
+export interface Product {
+  id?: string;
+  images: string[];
+  title: string;
+  price: number;
+  description: string;
 }
